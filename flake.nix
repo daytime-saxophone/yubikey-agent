a041da3cab30e986fb5a658706d3146e4c546950
Isak Johansson <isak.johansson@decentriq.ch>
2022-10-14 09:54:56 +0200

    fix: support old style defaults

--- flake.nix
+++ flake.nix
@@ -5,29 +5,37 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.gomod2nix.url = "github:nix-community/gomod2nix";
 
-  outputs = { self, nixpkgs, flake-utils, gomod2nix }:
-    (flake-utils.lib.eachSystem
-      [
-        "aarch64-linux"
-        "aarch64-darwin"
-        "i686-linux"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ]
-      (system:
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ gomod2nix.overlays.default ];
-          };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    gomod2nix,
+  }: (flake-utils.lib.eachSystem [
+      "aarch64-linux"
+      "aarch64-darwin"
+      "i686-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ]
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [gomod2nix.overlays.default];
+      };
 
-        in
-        {
-          #devShells.default = pkgs.mkShell {
-          #  buildInputs = with pkgs; [ gomod2nix ];
-          #};
-          packages.default = pkgs.callPackage ./. { };
-          devShells.default = import ./shell.nix { inherit pkgs self; };
-        })
-    );
+      yubikey-agent = pkgs.callPackage ./. {};
+      development = pkgs.mkShell {
+        packages = [
+          pkgs.gomod2nix
+          (pkgs.mkGoEnv {pwd = ./.;})
+          yubikey-agent
+        ];
+      };
+    in {
+      packages.default = yubikey-agent;
+      defaultPackage = yubikey-agent;
+
+      devShells.default = development;
+      devShell = development;
+    }));
 }
